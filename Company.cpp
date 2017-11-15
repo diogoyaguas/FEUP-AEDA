ba7e9d71650a9559a1aee9b2c3496993e8763287
Diogo Yaguas <diogo.yaguas@hotmail.com>
2017-11-15 00:46:10 +0000

    QuickSort almost completed
    Bea és linda

--- Company.cpp
+++ Company.cpp
@@ -38,3 +38,48 @@ Company::Company() {
 vector<Beach *> Company::getBeaches(){
     return Beaches;
 }
+
+const unsigned int &Company::median3(vector<Beach *> &v, unsigned int left, unsigned int right)
+{
+    unsigned int center = (left+right) /2;
+
+    if (v.at(center)->get_max_capacity() < v.at(left)->get_max_capacity())
+        swap(v.at(left)->get_max_capacity(), v.at(center)->get_max_capacity());
+
+    if (v.at(right)->get_max_capacity() < v.at(left)->get_max_capacity())
+        swap(v.at(left)->get_max_capacity(), v.at(right)->get_max_capacity());
+
+    if (v.at(right)->get_max_capacity() < v.at(center)->get_max_capacity())
+        swap(v.at(center)->get_max_capacity(), v.at(right)->get_max_capacity());
+
+
+    //place pivot in position right-1
+    swap(v.at(center-1)->get_max_capacity(), v.at(right-1)->get_max_capacity());
+    return v.at(right-1)->get_max_capacity();
+}
+
+void Company::quickSort(vector<Beach *> &v){
+
+    quickSort(v,0,v.size()-1);
+}
+
+void Company::quickSort(vector<Beach *> &v, unsigned int left, unsigned int right)
+{
+        int x = median3(v,left,right); // x is the new pivot
+        int i = left; int j = right-1; // passo de partição
+        for(; ; ) {
+            while (v.at(++i)->get_max_capacity() < x) ;
+            while (x < v.at(--j)->get_max_capacity()) ;
+            if (i < j)
+                swap(v.at(i), v.at(j));
+            else break;
+        }
+        swap(v.at(i)->get_max_capacity(), v.at(right-1)->get_max_capacity()); //restores pivot
+        quickSort(v, left, i-1);
+        quickSort(v, i+1, right);
+
+}
+
+
+
+
--- Company.h
+++ Company.h
@@ -9,7 +9,15 @@ private:
 public:
     Company();
     vector<Beach *> getBeaches();
+    void quickSort(vector<Beach *> &v);
+    void  quickSort(vector<Beach *> &v,unsigned int left,unsigned int right);
+    const unsigned int &median3(vector<Beach *> &v, unsigned int left, unsigned int right);
 };
 
+template <class T>
+void swap ( T& a, T& b )
+{
+    T c(a); a=b; b=c;
+}
 
 #endif //AEDA_COMPANY_H
